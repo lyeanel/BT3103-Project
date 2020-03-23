dd61f0f0e0d454cfe879def34b0872398f6f0e43
cheahcha <61797648+cheahcha@users.noreply.github.com>
2020-03-23 19:24:14 +0800

    node modules

--- src/main.js
+++ src/main.js
@@ -1,14 +1,13 @@
-import Vue from 'vue'
-import './plugins'
+import Vue from 'vue';
+import './plugins';
 import VueChatScroll from 'vue-chat-scroll';
-import App from './App'
-import router from './router'
-import store from './store'
-import { CarouselPlugin } from 'bootstrap-vue'
-import { NavbarPlugin } from 'bootstrap-vue'
-import './firebase/init'
+import App from './App';
+import router from './router';
+import store from './store';
+import { CarouselPlugin } from 'bootstrap-vue';
+import { NavbarPlugin } from 'bootstrap-vue';
+import './firebase/init';
 import firebase from 'firebase/app';
-
 
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -19,12 +18,12 @@ Vue.use(CarouselPlugin);
 Vue.use(NavbarPlugin);
 
 firebase.auth().onAuthStateChanged(user => {
-  store.dispatch("fetchUser", user);
+  store.dispatch('fetchUser', user);
 });
 
 new Vue({
-    render: h => h(App),
-    router,
-    store,
-    components: { App }
-}).$mount('#app')
+  render: h => h(App),
+  router,
+  store,
+  components: { App }
+}).$mount('#app');
